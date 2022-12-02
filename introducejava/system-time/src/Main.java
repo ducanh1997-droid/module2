8d12480a2428eed87658399a590e08cfb415c007
ducanh1997 <ducanh8098@gmail.com>
2022-12-02 10:53:32 +0700

    update

---
+++ introducejava/system-time/src/Main.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Main {
+  static LocalDate date = LocalDate.now();
+    public static void main(String[] args) {
+        //LocalDate now = new LocalDate(1999,10,1);
+        System.out.println("Date"+date);
+    }
+}
